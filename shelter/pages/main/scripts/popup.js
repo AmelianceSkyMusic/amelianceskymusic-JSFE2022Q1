9b0c9f2d12dc13fb92d5c1a1e97dc099b855cf77
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-04-23 23:33:56 +0300

    feat: add pop up for 1280

---
+++ shelter/pages/main/scripts/popup.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import {
+    msg as Msg,
+    // getRandomNumber,
+    createHTMLElem,
+    // createHTMLElemPrep,
+    // removeKeysFromArray,
+} from '../../../scripts/modules/asm.js';
+// import * as asm from 'ASM_js-unctions.js';
+
+// const petsCards = document.querySelectorAll('.pets__card');
+
+// for (const card of petsCards) {
+//     card.addEventListener('click', showInfo);
+// }
+
+
+// function showInfo(event) {
+//     const el = event.target;
+//     const currentCard = el.closest('.pets__card');
+//     const name = currentCard.querySelector('.h4').innerText;
+//     Msg(name)
+// }
