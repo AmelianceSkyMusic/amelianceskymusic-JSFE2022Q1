6aaea5b77174fe4a3ac7bd425686827d11155f23
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-29 10:17:02 +0300

    feat: add API key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2e5debdad61a4b9cb982ed04656c99f7', // получите свой ключ https://newsapi.org/
         });
     }
 }
