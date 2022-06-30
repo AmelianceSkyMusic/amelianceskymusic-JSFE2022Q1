7dffd0c12958c7064cf2ae3a95b130917b2a2eac
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-30 18:36:40 +0300

    refactor: loader js to ts

---
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -0,0 +1,56 @@
+import { LoadCallbackData } from '../types/interface';
+class Loader {
+    baseLink: string;
+    options: { apiKey: string };
+    constructor(baseLink: string, options: { apiKey: string }) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options }: { endpoint: string; options: { sources: string } },
+        // (data) => this.view.drawNews(data)
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+        console.log('res', res);
+        return res;
+    }
+
+    makeUrl(options: { sources: string }, endpoint: string) {
+        const urlOptions: { [key: string]: string } = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        // console.log('urlOptions', urlOptions);
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load(
+        method: string,
+        endpoint: string,
+        callback: (someData: LoadCallbackData) => void,
+        options: { sources: string }
+    ) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
