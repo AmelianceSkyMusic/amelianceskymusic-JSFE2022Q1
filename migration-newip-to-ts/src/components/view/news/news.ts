38df39886eed252d194f0e41ff3116924eeed22f
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-30 13:01:27 +0300

    refactor: news js to ts

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -18,38 +18,37 @@ class News {
     draw(data: NewsData[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
+        const fragment = document.createDocumentFragment(); // ? as DocumentFragment;
         const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
         // ? QUESTION TO MENTOR: The cleaner way would be to not use .forEach. It's almost never needed if you're using TypeScript or a modern version of JavaScript
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
-            if (idx % 2) (newsClone.querySelector('.news__item') as HTMLTemplateElement).classList.add('alt');
+            if (idx % 2) (newsClone.querySelector('.news__item') as HTMLDivElement).classList.add('alt');
 
-            (newsClone.querySelector('.news__meta-photo') as HTMLTemplateElement).style.backgroundImage = `url(${
+            (newsClone.querySelector('.news__meta-photo') as HTMLDivElement).style.backgroundImage = `url(${
                 item.urlToImage || 'img/news_placeholder.jpg'
             })`;
-            (newsClone.querySelector('.news__meta-author') as HTMLTemplateElement).textContent =
+            (newsClone.querySelector('.news__meta-author') as HTMLUListElement).textContent =
                 item.author || item.source.name;
-            (newsClone.querySelector('.news__meta-date') as HTMLTemplateElement).textContent = item.publishedAt
+            (newsClone.querySelector('.news__meta-date') as HTMLUListElement).textContent = item.publishedAt
                 .slice(0, 10)
                 .split('-')
                 .reverse()
                 .join('-');
 
-            (newsClone.querySelector('.news__description-title') as HTMLTemplateElement).textContent = item.title;
-            (newsClone.querySelector('.news__description-source') as HTMLTemplateElement).textContent =
-                item.source.name;
-            (newsClone.querySelector('.news__description-content') as HTMLTemplateElement).textContent =
+            (newsClone.querySelector('.news__description-title') as HTMLHeadingElement).textContent = item.title;
+            (newsClone.querySelector('.news__description-source') as HTMLHeadingElement).textContent = item.source.name;
+            (newsClone.querySelector('.news__description-content') as HTMLParagraphElement).textContent =
                 item.description;
-            (newsClone.querySelector('.news__read-more a') as HTMLTemplateElement).setAttribute('href', item.url);
+            (newsClone.querySelector('.news__read-more a') as HTMLLinkElement).setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        (document.querySelector('.news') as HTMLTemplateElement).innerHTML = '';
-        (document.querySelector('.news') as HTMLTemplateElement).appendChild(fragment);
+        (document.querySelector('.news') as HTMLDivElement).innerHTML = '';
+        (document.querySelector('.news') as HTMLDivElement).appendChild(fragment);
     }
 }
 
