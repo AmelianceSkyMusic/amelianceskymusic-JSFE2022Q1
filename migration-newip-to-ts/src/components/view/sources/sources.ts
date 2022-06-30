1eb3b921897c192ab09e0694b4c9f6203afeef22
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-30 13:01:36 +0300

    refactor: sources js to ts

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -12,21 +12,21 @@ interface SourcesData {
 
 class Sources {
     draw(data: SourcesData[]) {
-        const fragment = document.createDocumentFragment();
+        const fragment = document.createDocumentFragment(); // ? as DocumentFragment;
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
         // ? QUESTION TO MENTOR: The cleaner way would be to not use .forEach. It's almost never needed if you're using TypeScript or a modern version of JavaScript
         data.forEach((item) => {
             // console.log('forEach', item);
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
-            (sourceClone.querySelector('.source__item-name') as HTMLTemplateElement).textContent = item.name;
-            (sourceClone.querySelector('.source__item') as HTMLTemplateElement).setAttribute('data-source-id', item.id);
+            (sourceClone.querySelector('.source__item-name') as HTMLSpanElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLDivElement).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        (document.querySelector('.sources') as HTMLTemplateElement).append(fragment);
+        (document.querySelector('.sources') as HTMLDivElement).append(fragment);
     }
 }
 
