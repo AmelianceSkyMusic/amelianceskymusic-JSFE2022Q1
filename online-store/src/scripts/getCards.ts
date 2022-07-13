436b23b99e15388e9418b307f023b2fbda27ce08
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-13 12:58:36 +0300

    refactor: add error log

--- online-store/src/scripts/getCards.ts
+++ online-store/src/scripts/getCards.ts
@@ -1,4 +1,6 @@
 
+import devs from './devs';
+
 function getCards(): {[key: string]: string}[] {
 
 	const cards: {[key: string]: string}[] = [];
@@ -14,37 +16,42 @@ function getCards(): {[key: string]: string}[] {
 		const url = 'https://docs.google.com/spreadsheets/d/' + sheetID + '/gviz/tq?tqx=out:json&sheet=' + sheetTitle + '&range=' + sheetRange;
 		console.log(sheetID, sheetTitle, sheetRange);
 
-
 		const res = await fetch(url);
-		const textData = await res.text();
 
-		const dataJson = JSON.parse(textData.substring(47).slice(0, -2));
+		if (res.status === 404) {
+			devs.error('getGoogleSheetsData() get 404');
+		} else {
+			const textData = await res.text();
 
-		// *----- convert data from simpla table when first row is heading -----
-		const dataObj: {[key: string]: string[]} = {};
-		const table = dataJson.table;
-		console.log(table);
-		const maxLengthOfColumn: number = table.rows[0].c.length;
-		for (let i = 0; i < maxLengthOfColumn; i++) {
-			const colDate = [];
-			for (let j = 1; j < table.rows.length; j++) {
-				if (table.rows[j].c[i]) {
-					colDate[j-1] = table.rows[j].c[i].v ?? '';
-				} else {
-					colDate[j] = '';
+			const dataJson = JSON.parse(textData.substring(47).slice(0, -2));
+
+			// *----- convert data from simpla table when first row is heading -----
+			const dataObj: {[key: string]: string[]} = {};
+			const table = dataJson.table;
+			console.log(table);
+			const maxLengthOfColumn: number = table.rows[0].c.length;
+			for (let i = 0; i < maxLengthOfColumn; i++) {
+				const colDate = [];
+				for (let j = 1; j < table.rows.length; j++) {
+					if (table.rows[j].c[i]) {
+						colDate[j-1] = table.rows[j].c[i].v ?? '';
+					} else {
+						colDate[j] = '';
+					}
 				}
+				dataObj[table.rows[0].c[i].v] = colDate;
 			}
-			dataObj[table.rows[0].c[i].v] = colDate;
+
+			// *----- convert data object heading based to cards array index based-----
+			for (let i = 0; i < maxLengthOfColumn - 1; i++) {
+				const card: {[key: string]: string} = {};
+				Object.keys(dataObj).forEach(el => {
+					card[el] = dataObj[el][i] || '';
+				});
+				cards.push (card);
+			}
 		}
 
-		// *----- convert data object heading based to cards array index based-----
-		for (let i = 0; i < maxLengthOfColumn - 1; i++) {
-			const card: {[key: string]: string} = {};
-			Object.keys(dataObj).forEach(el => {
-				card[el] = dataObj[el][i] || '';
-			});
-			cards.push (card);
-		}
 	}
 
 	getGoogleSheetsData(sheetID, sheetTitle, sheetRange);
