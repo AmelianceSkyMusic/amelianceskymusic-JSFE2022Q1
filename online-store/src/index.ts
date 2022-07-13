384b4df1f06e49e64a7a78460329f35978a96a3c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-13 19:59:42 +0300

    refactor: clean code

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,19 +1,8 @@
 import './scss/_themes/dark.scss';
 import './style.scss';
 
-import rangeSliderDouble from './scripts/rangeSliderDouble';
+import App from './scripts/controllers/App';
 
-const siderStockBalance$ = document.querySelector('#range-slider-double-stock-balance') as HTMLDivElement;
-rangeSliderDouble(siderStockBalance$, {
-	labelPrefix1: '$ ',
-	labelPrefix2: '$ ',
-	range: [10, 300],
-	startValues: [20, 100],
-	step: 5,
-});
+const app = new App();
 
-const siderYear$ = document.querySelector('#range-slider-double-year') as HTMLDivElement;
-rangeSliderDouble(siderYear$, {
-	range: [2014, 2022],
-	startValues: [2020, 2022],
-});
+app.start();
