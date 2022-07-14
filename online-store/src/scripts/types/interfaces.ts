666272bc4519889429b7dfcde6af0e70a70bd39b
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-14 17:00:42 +0300

    refactor: update Interface ICard

--- online-store/src/scripts/types/interfaces.ts
+++ online-store/src/scripts/types/interfaces.ts
@@ -1,13 +1,14 @@
 
 export interface ICard {
+	price?: string,
     name?: string,
-    stockBalance?: string
-    releseYear?: string
-    color?: string
-    size?: string
-    popular?: string
-    brand?: string
-    image?: string
-    inCart?: number
-    id?: number
+    balance?: string,
+    year?: string,
+    color?: string,
+    size?: string,
+    popular?: string,
+    brand?: string,
+    image?: string,
+    inCart?: number,
+    id?: number,
 }
