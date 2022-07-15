31e98c407f3cd8c612301044a4201e4724ab413e
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-15 20:55:49 +0300

    feat: add checkbox filters

--- online-store/src/controllers/App.ts
+++ online-store/src/controllers/App.ts
@@ -2,11 +2,25 @@ import { shuffleArray } from '../asm-scripts';
 import AppModel from '../models/AppModel';
 import { ICard } from '../types/interfaces';
 import AppView from '../views/AppView';
+
+interface IFilters {
+	price: number[],
+	balance: number[],
+	year: number[],
+	brand: string[],
+	popular: string[],
+	color: string[],
+	size: string[],
+
+}
+
 export default class App {
 	state: { [key: string]: string };
 	initData: ICard[] = [];
 	data: ICard[] = [];
-	view = new AppView();
+	view: AppView;
+	filters: IFilters;
+	// filters: { {[key: string]: string[]}{}  };
 	constructor() {
 		this.state = {
 			sheetID: '11IF6n311xG3ycdE_mOQaZizL7NFzeynvFu2ni1sghQ0',
@@ -16,6 +30,16 @@ export default class App {
 				return 'https://docs.google.com/spreadsheets/d/' + this.sheetID +
 						'/gviz/tq?tqx=out:json&sheet=' + this.sheetTitle + '&range=' + this.sheetRange;
 			}
+		};
+		this.view = new AppView();
+		this.filters = {
+			'price': [],
+			'year': [],
+			'balance': [],
+			'brand': [],
+			'popular': [],
+			'color': [],
+			'size': []
 		};
 	}
 
@@ -28,6 +52,40 @@ export default class App {
 		this.data = this.initData;
 
 		this.search();
+		this.filter();
+		this.applyFilters();
+	}
+
+	applyFilters() {
+
+		this.data = this.initData;
+
+		const filterByValues = (dataArray: ICard[], property: string, valuesArray: string[]): ICard[] => {
+			const resultData: ICard[] = [];
+			if (valuesArray.length > 0) {
+				valuesArray.forEach( value => {
+					for (const cardObj of dataArray) {
+						const isInData = Boolean(resultData.find(item => {
+							return cardObj.id === item.id;
+						}));
+
+						const prop = cardObj[property as keyof ICard] || [];
+						if(!isInData && prop.toString().toLocaleLowerCase().includes((value).toLocaleLowerCase())) {
+							resultData.push(cardObj);
+						}
+					}
+				});
+				return resultData;
+			} else {
+				return dataArray;
+			}
+		};
+		this.data = [...filterByValues(this.data, 'brand', this.filters.brand)];
+		this.data = [...filterByValues(this.data, 'popular', this.filters.popular)];
+		this.data = [...filterByValues(this.data, 'color', this.filters.color)];
+		this.data = [...filterByValues(this.data, 'size', this.filters.size)];
+
+		this.view.render(this.data);
 	}
 
 	search() {
@@ -36,11 +94,36 @@ export default class App {
 			const target = event.target as HTMLInputElement;
 			const resultData: ICard[] = [];
 			for (const cardObj of this.data) {
-				if(cardObj.name?.toString().toLocaleLowerCase().includes((target.value.toLocaleLowerCase()))) {
+				if(cardObj.name?.toString().toLocaleLowerCase().includes(target.value.toLocaleLowerCase())) {
 					resultData.push(cardObj);
 				}
 			}
 			this.view.render(resultData);
 		});
+	}
+	filter() {
+
+		const getCheckboxFilter = (event: Event) => {
+			const target = event.target as HTMLInputElement;
+			const inputNameArr = target.name.split('__');
+			const inputName = inputNameArr[inputNameArr.length - 1];
+			const isChecked = target.checked;
+			const params = this.filters[inputName as keyof IFilters] as string[];
+			if (isChecked) {
+				params.push(target.value);
+			} else {
+				params.splice(params.indexOf(target.value), 1);
+			}
+			this.applyFilters();
+
+		};
+		const filterBrandD = document.querySelector('.filter__brand');
+		const filterPopularityD = document.querySelector('.filter__popularity');
+		const filterColorD = document.querySelector('.filter__color');
+		const filterSizeD = document.querySelector('.filter__size');
+		filterBrandD?.addEventListener('input', getCheckboxFilter);
+		filterColorD?.addEventListener('input', getCheckboxFilter);
+		filterPopularityD?.addEventListener('input', getCheckboxFilter);
+		filterSizeD?.addEventListener('input', getCheckboxFilter);
 	}
 }
