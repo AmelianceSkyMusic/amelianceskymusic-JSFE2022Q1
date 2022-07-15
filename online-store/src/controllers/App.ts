a0e6c079c67defbe0eec1bdd4dfceb7bd9b3c2ee
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-15 14:26:26 +0300

    feat: add search

--- online-store/src/controllers/App.ts
+++ online-store/src/controllers/App.ts
@@ -1,7 +1,12 @@
+import { shuffleArray } from '../asm-scripts';
 import AppModel from '../models/AppModel';
+import { ICard } from '../types/interfaces';
 import AppView from '../views/AppView';
 export default class App {
 	state: { [key: string]: string };
+	initData: ICard[] = [];
+	data: ICard[] = [];
+	view = new AppView();
 	constructor() {
 		this.state = {
 			sheetID: '11IF6n311xG3ycdE_mOQaZizL7NFzeynvFu2ni1sghQ0',
@@ -16,7 +21,24 @@ export default class App {
 	async start() {
 		const model = new AppModel(this.state);
 		const data = await model.getCards();
-		const view = new AppView(data);
-		view.render();
+		this.initData = shuffleArray(data);
+		this.view = new AppView();
+		this.view.render(this.initData);
+		this.data = this.initData;
+
+		this.search();
+	}
+
+	search() {
+		const searchD = document.querySelector('.cards-search');
+		searchD?.addEventListener('input', (event) => {
+			const target = event.target as HTMLInputElement;
+			const resultData: ICard[] = [];
+			for (const cardObj of this.data) {
+
+				if(cardObj.name?.includes(target.value)) resultData.push(cardObj);
+			}
+			this.view.render(resultData);
+		});
 	}
 }
