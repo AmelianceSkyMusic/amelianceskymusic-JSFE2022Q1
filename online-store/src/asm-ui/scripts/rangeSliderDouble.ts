12fdcb8036cdb743a65ff35534496a6f59e327fa
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-16 11:26:07 +0300

    feat: add reset button

--- online-store/src/asm-ui/scripts/rangeSliderDouble.ts
+++ online-store/src/asm-ui/scripts/rangeSliderDouble.ts
@@ -8,7 +8,7 @@ interface IRangeSliderDoubleObj {
 	labelPostfix2?: string,
 	label1?: boolean,
 	label2?: boolean,
-	step?: [number, number];
+	step?: number[];
 }
 
 export class RangeSliderDouble {
@@ -21,10 +21,19 @@ export class RangeSliderDouble {
 	labelPostfix2?: string;
 	label1?: boolean;
 	label2?: boolean;
-	step?: [number, number];
-	constructor(
-		public siderComponentD: HTMLElement,
-		{
+	step?: number[];
+	siderComponent$: HTMLElement;
+
+	slider1$: HTMLInputElement;
+	slider2$: HTMLInputElement;
+	labels$: HTMLSpanElement;
+	label1$: HTMLSpanElement;
+	label2$: HTMLSpanElement;
+	sliderTrack$: HTMLDivElement;
+	rgbaSliderTrackColorsArray: number[];
+
+	constructor (
+		siderComponent$: HTMLElement, {
 			minGap = 1,
 			range = [0, 100],
 			startValues = [...range],
@@ -34,9 +43,9 @@ export class RangeSliderDouble {
 			labelPostfix2 = '',
 			label1 = true,
 			label2 = true,
-			step,
-		}: IRangeSliderDoubleObj
-	) {
+			step = []
+		}: IRangeSliderDoubleObj) {
+
 		this.minGap = minGap;
 		this.range = range;
 		this.startValues = startValues;
@@ -47,25 +56,47 @@ export class RangeSliderDouble {
 		this.label1 = label1;
 		this.label2 = label2;
 		this.step = step;
+
+		this.siderComponent$ = siderComponent$;
+		this.slider1$ = this.siderComponent$.querySelector('#range-slider-double__slider-1') as HTMLInputElement;
+		this.slider2$ = this.siderComponent$.querySelector('#range-slider-double__slider-2') as HTMLInputElement;
+		this.labels$ = this.siderComponent$.querySelector('#range-slider-double__labels') as HTMLSpanElement;
+		this.label1$ = this.siderComponent$.querySelector('#range-slider-double__label-1') as HTMLSpanElement;
+		this.label2$ = this.siderComponent$.querySelector('#range-slider-double__label-2') as HTMLSpanElement;
+		this.sliderTrack$ = this.siderComponent$.querySelector('#range-slider-double__track') as HTMLDivElement;
+
+		this.rgbaSliderTrackColorsArray = [];
 	}
 
+	getSliderColor() {
+		const rgbaSliderTrackColor: string = window.getComputedStyle(this.sliderTrack$).backgroundColor;
+		this.rgbaSliderTrackColorsArray = rgbaSliderTrackColor.split(',').map((elem): number => parseInt(elem.replace(/^\D+/g, '')));
+		if (this.rgbaSliderTrackColorsArray.length <= 3) this.rgbaSliderTrackColorsArray.push(1);
+	}
 	render(): [HTMLInputElement, HTMLInputElement] {
 
-		const slider1$ = this.siderComponentD.querySelector('#range-slider-double__slider-1') as HTMLInputElement;
-		const slider2$ = this.siderComponentD.querySelector('#range-slider-double__slider-2') as HTMLInputElement;
-		const labels$ = this.siderComponentD.querySelector('#range-slider-double__labels') as HTMLSpanElement;
-		const label1$ = this.siderComponentD.querySelector('#range-slider-double__label-1') as HTMLSpanElement;
-		const label2$ = this.siderComponentD.querySelector('#range-slider-double__label-2') as HTMLSpanElement;
-		const sliderTrack$ = this.siderComponentD.querySelector('#range-slider-double__track') as HTMLDivElement;
-		const rgbaSliderTrackColor: string = window.getComputedStyle(sliderTrack$).backgroundColor;
+		this.getSliderColor();
+
+		const slider1$ = this.slider1$;
+		const slider2$ = this.slider2$;
+		const labels$ = this.labels$;
+		const label1$ = this.label1$;
+		const label2$ = this.label2$;
+
+
+		const sliderTrack$ = this.siderComponent$.querySelector('#range-slider-double__track') as HTMLDivElement;
+		const [r, g, b, a] = this.rgbaSliderTrackColorsArray;
 		sliderTrack$.style.backgroundColor = 'none';
+
 		const range = this.range as number[];
 		const minGap = this.minGap as number;
+		const step = this.step as number[];
 
-		if (this.step) {
-			slider1$.step = this.step[0].toString();
-			slider2$.step = this.step[1].toString();
+		if (step?.length > 0) {
+			slider1$.step = step[0].toString();
+			slider2$.step = step[1].toString();
 		}
+
 		slider1$.dataset.max = (range[1] - minGap).toString();
 		slider2$.dataset.min = (range[0] + minGap).toString();
 
@@ -83,17 +114,13 @@ export class RangeSliderDouble {
 			const percent1: number = Math.trunc((+slider1$.value - range[0]) * 100 / (range[1] - range[0]));
 			const percent2: number = Math.trunc((+slider2$.value - range[0]) * 100 / (range[1] - range[0]));
 
-			const rgbaSliderTrackColorArray: number[] = rgbaSliderTrackColor.split(',').map((elem): number => parseInt(elem.replace(/^\D+/g, '')));
-			if (rgbaSliderTrackColorArray.length <= 3) rgbaSliderTrackColorArray.push(1);
-			const [r, g, b, a] = rgbaSliderTrackColorArray;
-
 			const linearGradient = `linear-gradient(to right, rgba(0, 0, 0, 0) ${percent1}%, rgba(${r}, ${g}, ${b}, ${a}) ${percent1}%,
 									rgba(${r}, ${g}, ${b}, ${a}) ${percent2}%, rgba(0, 0, 0, 0) ${percent2}%)`;
 
 			sliderTrack$.style.background = linearGradient;
 		};
 
-		const updateSlider1 = (): string => {
+		const updateSlider1 = () => {
 			if (parseInt(slider2$.value) - parseInt(slider1$.value) <= minGap) {
 				slider1$.value = (parseInt(slider2$.value) - minGap) + '';
 			}
@@ -107,11 +134,9 @@ export class RangeSliderDouble {
 			label1$.textContent = labelValue;
 
 			fillColor();
-
-			return labelValue;
 		};
 
-		const updateSlider2 = (): string => {
+		const updateSlider2 = () => {
 			if (parseInt(slider2$.value) - parseInt(slider1$.value) <= minGap) {
 				slider2$.value = (parseInt(slider1$.value) + minGap) + '';
 			}
@@ -124,8 +149,6 @@ export class RangeSliderDouble {
 			label2$.textContent = labelValue;
 
 			fillColor();
-
-			return labelValue;
 		};
 
 		slider1$.addEventListener('input', updateSlider1);
@@ -134,9 +157,65 @@ export class RangeSliderDouble {
 		updateSlider1();
 		updateSlider2();
 
+		return [slider1$, slider2$];
+	}
+
+	update() {
+
+		const slider1$ = this.slider1$;
+		const slider2$ = this.slider2$;
+		const label1$ = this.label1$;
+		const label2$ = this.label2$;
+		const sliderTrack$ = this.siderComponent$.querySelector('#range-slider-double__track') as HTMLDivElement;
+		const [r, g, b, a] = this.rgbaSliderTrackColorsArray;
+		const range = this.range as number[];
+		const minGap = this.minGap as number;
+		slider1$.value = range[0].toString();
+		slider2$.value = range[1].toString();
+
+		const fillColor = (): void => {
+			const percent1: number = Math.trunc((+slider1$.value - range[0]) * 100 / (range[1] - range[0]));
+			const percent2: number = Math.trunc((+slider2$.value - range[0]) * 100 / (range[1] - range[0]));
+
+			const linearGradient = `linear-gradient(to right, rgba(0, 0, 0, 0) ${percent1}%, rgba(${r}, ${g}, ${b}, ${a}) ${percent1}%,
+										rgba(${r}, ${g}, ${b}, ${a}) ${percent2}%, rgba(0, 0, 0, 0) ${percent2}%)`;
+
+			sliderTrack$.style.background = linearGradient;
+		};
+
+		const updateSlider1 = () => {
+			if (parseInt(slider2$.value) - parseInt(slider1$.value) <= minGap) {
+				slider1$.value = (parseInt(slider2$.value) - minGap) + '';
+			}
+
+			const labelValueNumb: number = +slider1$.value;
+			let labelValue: string = labelValueNumb.toString();
+			if (this.labelPrefix1) labelValue = this.labelPrefix1 + labelValue;
+			if (this.labelPostfix1) labelValue = labelValue + this.labelPostfix1;
+
+			label1$.textContent = labelValue;
+
+		};
+
+		const updateSlider2 = () => {
+			if (parseInt(slider2$.value) - parseInt(slider1$.value) <= minGap) {
+				slider2$.value = (parseInt(slider1$.value) + minGap) + '';
+			}
+
+			const labelValueNumb: number = +slider2$.value;
+			let labelValue: string = labelValueNumb.toString();
+			if (this.labelPrefix2) labelValue = this.labelPrefix2 + labelValue;
+			if (this.labelPostfix2) labelValue = labelValue + this.labelPostfix2;
+
+			label2$.textContent = labelValue;
+
+		};
+
+		updateSlider1();
+		updateSlider2();
+
 		fillColor();
 
-		return [slider1$, slider2$];
 	}
 
 }
--- online-store/src/controllers/App.ts
+++ online-store/src/controllers/App.ts
@@ -21,6 +21,9 @@ export default class App {
 	data: ICard[] = [];
 	view: AppView;
 	filters: IFilters;
+	rangeSliderDoublePrice: RangeSliderDouble | undefined;
+	rangeSliderDoubleYear: RangeSliderDouble | undefined;
+	rangeSliderDoubleBalance: RangeSliderDouble | undefined;
 
 	constructor() {
 		this.state = {
@@ -45,7 +48,9 @@ export default class App {
 	}
 
 	async start() {
-		this.addRangeFilters();
+		this.rangeFilters();
+		this.checkboxFilter();
+		this.resetFilters();
 		const model = new AppModel(this.state);
 		const data = await model.getCards();
 		this.initData = shuffleArray(data);
@@ -54,10 +59,49 @@ export default class App {
 		this.data = this.initData;
 
 		this.search();
-		this.filter();
 	}
 
-	addRangeFilters() {
+	search() {
+		const searchD = document.querySelector('.cards-search');
+		searchD?.addEventListener('input', (event) => {
+			const target = event.target as HTMLInputElement;
+			const resultData: ICard[] = [];
+			for (const cardObj of this.data) {
+				if(cardObj.name?.toString().toLocaleLowerCase().includes(target.value.toLocaleLowerCase())) {
+					resultData.push(cardObj);
+				}
+			}
+			this.view.render(resultData);
+		});
+	}
+
+	checkboxFilter() {
+
+		const getCheckboxFilter = (event: Event) => {
+			const target = event.target as HTMLInputElement;
+			const inputNameArr = target.name.split('__');
+			const inputName = inputNameArr[inputNameArr.length - 1];
+			const isChecked = target.checked;
+			const params = this.filters[inputName as keyof IFilters] as string[];
+			if (isChecked) {
+				params.push(target.value);
+			} else {
+				params.splice(params.indexOf(target.value), 1);
+			}
+			this.applyFilters();
+		};
+
+		const filterBrandD = document.querySelector('.filter__brand');
+		const filterPopularityD = document.querySelector('.filter__popularity');
+		const filterColorD = document.querySelector('.filter__color');
+		const filterSizeD = document.querySelector('.filter__size');
+		filterBrandD?.addEventListener('input', getCheckboxFilter);
+		filterColorD?.addEventListener('input', getCheckboxFilter);
+		filterPopularityD?.addEventListener('input', getCheckboxFilter);
+		filterSizeD?.addEventListener('input', getCheckboxFilter);
+	}
+
+	rangeFilters() {
 
 		const getRangeSliderDoubleFilter = (event: Event, ) => {
 			const target$ = event.currentTarget as HTMLInputElement;
@@ -80,30 +124,34 @@ export default class App {
 		};
 
 		const siderStockPrice$ = document.querySelector('#range-slider-double__price') as HTMLDivElement;
-		new RangeSliderDouble(siderStockPrice$, {
+		const siderYear$ = document.querySelector('#range-slider-double__year') as HTMLDivElement;
+		const siderBalance$ = document.querySelector('#range-slider-double__balance') as HTMLDivElement;
+
+		this.rangeSliderDoublePrice = new RangeSliderDouble (siderStockPrice$, {
 			labelPrefix1: '$ ',
 			labelPrefix2: '$ ',
 			range: [60, 150],
 			step: [10, 10],
 			minGap: 10
-		}).render();
-		siderStockPrice$.addEventListener('input', getRangeSliderDoubleFilter, true);
-
-		const siderYear$ = document.querySelector('#range-slider-double__year') as HTMLDivElement;
-		new RangeSliderDouble(siderYear$, {
+		});
+		this.rangeSliderDoubleYear = new RangeSliderDouble (siderYear$, {
 			range: [2014, 2022],
 			startValues: [2014, 2022],
 			minGap: 1,
-		}).render();
-		siderYear$.addEventListener('input', getRangeSliderDoubleFilter);
-
-		const siderBalance$ = document.querySelector('#range-slider-double__balance') as HTMLDivElement;
-		new RangeSliderDouble(siderBalance$, {
+		});
+		this.rangeSliderDoubleBalance = new RangeSliderDouble (siderBalance$, {
 			range: [1, 61],
 			startValues: [1, 61],
 			step: [5, 5],
 			minGap: 10,
-		}).render();
+		});
+
+		this.rangeSliderDoublePrice.render();
+		this.rangeSliderDoubleYear.render();
+		this.rangeSliderDoubleBalance.render();
+
+		siderStockPrice$.addEventListener('input', getRangeSliderDoubleFilter);
+		siderYear$.addEventListener('input', getRangeSliderDoubleFilter);
 		siderBalance$.addEventListener('input', getRangeSliderDoubleFilter);
 
 	}
@@ -160,43 +208,34 @@ export default class App {
 		this.view.render(this.data);
 	}
 
-	search() {
-		const searchD = document.querySelector('.cards-search');
-		searchD?.addEventListener('input', (event) => {
-			const target = event.target as HTMLInputElement;
-			const resultData: ICard[] = [];
-			for (const cardObj of this.data) {
-				if(cardObj.name?.toString().toLocaleLowerCase().includes(target.value.toLocaleLowerCase())) {
-					resultData.push(cardObj);
-				}
-			}
-			this.view.render(resultData);
+	resetFilters() {
+		const resetButton$ = document.querySelector('#filter__reset-button');
+		resetButton$?.addEventListener('click', () => {
+			this.data = this.initData;
+			const filterBrand$ = document.querySelectorAll('.filter__brand [type="checkbox"]') as NodeListOf<HTMLInputElement>;
+			const filterPopularity$ = document.querySelectorAll('.filter__popularity [type="checkbox"]') as NodeListOf<HTMLInputElement>;
+			const filterColor$ = document.querySelectorAll('.filter__color [type="checkbox"]') as NodeListOf<HTMLInputElement>;
+			const filterSize$ = document.querySelectorAll('.filter__size [type="checkbox"]') as NodeListOf<HTMLInputElement>;
+			filterBrand$.forEach(elem => elem.checked = false);
+			filterPopularity$.forEach(elem => elem.checked = false);
+			filterColor$.forEach(elem => elem.checked = false);
+			filterSize$.forEach(elem => elem.checked = false);
+			const siderStockPrice1$ = document.querySelector('#range-slider-double__price #range-slider-double__slider-1') as HTMLInputElement;
+			const siderStockPrice2$ = document.querySelector('#range-slider-double__price #range-slider-double__slider-2') as HTMLInputElement;
+			const siderYear1$ = document.querySelector('#range-slider-double__year #range-slider-double__slider-1') as HTMLInputElement;
+			const siderYear2$ = document.querySelector('#range-slider-double__year #range-slider-double__slider-2') as HTMLInputElement;
+			const siderBalance1$ = document.querySelector('#range-slider-double__balance #range-slider-double__slider-1') as HTMLInputElement;
+			const siderBalance2$ = document.querySelector('#range-slider-double__balance #range-slider-double__slider-2') as HTMLInputElement;
+			siderStockPrice1$.value = siderStockPrice1$.min;
+			siderStockPrice2$.value = siderStockPrice2$.max;
+			siderYear1$.value = siderYear1$.min;
+			siderYear2$.value = siderYear2$.max;
+			siderBalance1$.value = siderBalance1$.min;
+			siderBalance2$.value = siderBalance2$.max;
+			(this.rangeSliderDoublePrice as RangeSliderDouble).update();
+			(this.rangeSliderDoubleYear as RangeSliderDouble).update();
+			(this.rangeSliderDoubleBalance as RangeSliderDouble).update();
+			this.view.render(this.data);
 		});
-	}
-
-	filter() {
-
-		const getCheckboxFilter = (event: Event) => {
-			const target = event.target as HTMLInputElement;
-			const inputNameArr = target.name.split('__');
-			const inputName = inputNameArr[inputNameArr.length - 1];
-			const isChecked = target.checked;
-			const params = this.filters[inputName as keyof IFilters] as string[];
-			if (isChecked) {
-				params.push(target.value);
-			} else {
-				params.splice(params.indexOf(target.value), 1);
-			}
-			this.applyFilters();
-		};
-
-		const filterBrandD = document.querySelector('.filter__brand');
-		const filterPopularityD = document.querySelector('.filter__popularity');
-		const filterColorD = document.querySelector('.filter__color');
-		const filterSizeD = document.querySelector('.filter__size');
-		filterBrandD?.addEventListener('input', getCheckboxFilter);
-		filterColorD?.addEventListener('input', getCheckboxFilter);
-		filterPopularityD?.addEventListener('input', getCheckboxFilter);
-		filterSizeD?.addEventListener('input', getCheckboxFilter);
 	}
 }
