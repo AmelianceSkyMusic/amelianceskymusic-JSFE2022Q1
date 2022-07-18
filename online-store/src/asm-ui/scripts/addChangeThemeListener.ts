699d1a4620abba1621714a4565f688177113b62e
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-18 20:30:55 +0300

    refactor: add theme listener file

---
+++ online-store/src/asm-ui/scripts/addChangeThemeListener.ts
@@ -0,0 +1,19 @@
+export const addChangeThemeListener = () => {
+	const btnTheme$ = document.querySelector('#button-theme') as HTMLButtonElement;
+
+	btnTheme$.addEventListener('click', (): void => {
+		if (btnTheme$.classList.contains('current-dark')) {
+			btnTheme$.classList.remove('current-dark');
+			btnTheme$.classList.add('current-light');
+			(document.querySelector('link[href*="theme-dark"]') as HTMLLinkElement).media = 'not all';
+			(document.querySelector('link[href*="theme-light"]') as HTMLLinkElement).media = 'all';
+			console.log('light');
+		} else {
+			btnTheme$.classList.remove('current-light');
+			btnTheme$.classList.add('current-dark');
+			(document.querySelector('link[href*="theme-dark"]') as HTMLLinkElement).media = 'all';
+			(document.querySelector('link[href*="theme-light"]') as HTMLLinkElement).media = 'not all';
+			console.log('dark');
+		}
+	});
+};
