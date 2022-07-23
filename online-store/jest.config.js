2fef1a03bad28a37d15563edb9082790bd97dd28
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-23 13:34:15 +0300

    refactor: update jest config

--- online-store/jest.config.js
+++ online-store/jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	transform: {
+		'^.+\\.(t|j)sx?$': '@swc/jest',
+	},
+	testEnvironment: 'node',
+	maxWorkers: 1,
+	roots: [
+		'<rootDir>/tests'
+	]
 };
