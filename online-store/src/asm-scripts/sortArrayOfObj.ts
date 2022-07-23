388177dbdec426db8d4b8730b65dd4ff30304dc9
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-23 13:35:57 +0300

    refactor: sortArrayOfObj.ts

--- online-store/src/asm-scripts/sortArrayOfObj.ts
+++ online-store/src/asm-scripts/sortArrayOfObj.ts
@@ -1,23 +1,13 @@
-export function sortArrayOfObj<T>(array: T[], key: string, type: string): T[]{
+export function sortArrayOfObj<T>(array: T[], key: keyof T, type: string): T[]{
 	const arr: T[] = [...array];
 	if (key) {
 		arr.sort((a: T, b: T): number => {
-			const aEl = (a[key as keyof T]);
-			const bEl = (b[key as keyof T]);
 			if (type === 'num')  {
-				if ( +aEl < +bEl){
-					return -1;
-				}
-				if ( +aEl > +bEl){
-					return 1;
-				}
+				if ( +a[key] < +b[key]) { return -1; }
+				if ( +a[key] > +b[key]) { return 1; }
 			} else {
-				if ( aEl < bEl){
-					return -1;
-				}
-				if ( aEl > bEl){
-					return 1;
-				}
+				if ( a[key] < b[key]) { return -1; }
+				if ( a[key] > b[key]) { return 1; }
 			}
 			return 0;
 		});
