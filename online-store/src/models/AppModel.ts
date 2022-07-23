3f94ac71f61a1d4ff30a7b066b6ade2573cb5dc8
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-23 13:37:06 +0300

    refactor: AppModel.ts

--- online-store/src/models/AppModel.ts
+++ online-store/src/models/AppModel.ts
@@ -1,14 +1,34 @@
 import { devs } from '../asm-scripts';
+import { IGoogleSheetsData } from '../types/IGoogleSheetsData';
 import { ICard } from '../types/interfaces';
 
 export default class AppModel {
+
 	constructor(public state: {[key: string]: string}) {}
+
+	static convertDataFromTableWithFirstRowHeading(data: IGoogleSheetsData) {
+		const dataObj: { [key: string]: string[] } = {};
+		const table = data.table;
+		const maxLengthOfColumn: number = table.rows[0].c.length;
+		for (let i = 0; i < maxLengthOfColumn; i++) {
+			const colDate = [];
+			for (let j = 1; j < table.rows.length; j++) {
+				if (table.rows[j].c[i]) {
+					colDate[j-1] = table.rows[j].c[i].v ?? '';
+				} else {
+					colDate[j] = '';
+				}
+			}
+			dataObj[table.rows[0].c[i].v] = colDate;
+		}
+		return dataObj;
+	}
 
 	static async getGoogleSheetsData(url: string): Promise<{ [key: string]: string[]; }> {
 
 		const response = await fetch(url);
 
-		const dataObj: { [key: string]: string[] } = {};
+		let dataObj: { [key: string]: string[] } = {};
 
 		if (response.status === 404) {
 			devs.error('getGoogleSheetsData() get 404');
@@ -17,35 +37,16 @@ export default class AppModel {
 			const textData = await response.text();
 			const dataJson = JSON.parse(textData.substring(47).slice(0, -2));
 
-			// *----- convert data from simple table when detect first row as heading -----
-			const table = dataJson.table;
-			const maxLengthOfColumn: number = table.rows[0].c.length;
-			for (let i = 0; i < maxLengthOfColumn; i++) {
-				const colDate = [];
-				for (let j = 1; j < table.rows.length; j++) {
-					if (table.rows[j].c[i]) {
-						colDate[j-1] = table.rows[j].c[i].v ?? '';
-					} else {
-						colDate[j] = '';
-					}
-				}
-				dataObj[table.rows[0].c[i].v] = colDate;
-			}
+			dataObj = { ...AppModel.convertDataFromTableWithFirstRowHeading(dataJson)};
 		}
 
 		return dataObj;
 	}
 
-	async getCards() {
-		const { url } = this.state;
-
-		const dataObj = await AppModel.getGoogleSheetsData(url);
-
+	static convertDataWithFirstRowHeadingToCardsArray(dataObj: {[key: string]: string[]}) {
 		const cards: ICard[] = [];
-
-		// *----- convert data object heading based to cards array index based-----
 		for (let i = 0; i < dataObj.id.length; i++) {
-			const card: { [key: string]: string | number } = {}; // TODO: rewrite to ICard
+			const card: { [key: string]: string | number } = {};
 			Object.keys(dataObj).forEach(el => {
 				card[el] = dataObj[el][i];
 			});
@@ -54,8 +55,12 @@ export default class AppModel {
 
 			cards.push(card);
 		}
-
 		return cards;
-
+	}
+	async getCards() {
+		const { url } = this.state;
+		const dataObj = await AppModel.getGoogleSheetsData(url);
+		const cards: ICard[] = [...AppModel.convertDataWithFirstRowHeadingToCardsArray(dataObj)];
+		return cards;
 	}
 }
