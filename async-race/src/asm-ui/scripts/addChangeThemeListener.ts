87c23d25d1bae59aa988e76c6dbcc5fa0e29bf06
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-07-30 23:01:58 +0300

    refactor: addChangeThemeListener

--- async-race/src/asm-ui/scripts/addChangeThemeListener.ts
+++ async-race/src/asm-ui/scripts/addChangeThemeListener.ts
@@ -1,9 +1,13 @@
 const toogleTheme = () => {
+  document.body.classList.add('no-transition');
+
   const currentTheme$ = document.documentElement.getAttribute('data-theme');
 
   let targetTheme = 'light';
   if (currentTheme$ === 'light') targetTheme = 'dark';
   document.documentElement.setAttribute('data-theme', targetTheme);
+
+  setTimeout(() => document.body.classList.remove('no-transition'), 0);
 
   localStorage.setItem('theme', targetTheme);
 };
