b8366d08de397ecc3c127bec45a0d5f71b985b42
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-03 10:18:22 +0300

    refactor: add renderHeader

--- async-race/src/app/appView/renderHeader.ts
+++ async-race/src/app/appView/renderHeader.ts
@@ -1,11 +1,20 @@
 import { createHTMLElem } from '../../asm-scripts';
 import { addChangeThemeListener } from '../../asm-ui/scripts';
+import { createCar } from '../API/createCar';
+import { deleteCar } from '../API/deleteCar';
+import { getCar } from '../API/getCar';
+import { getCars } from '../API/getCars';
+import { startCarSEngine } from '../API/startCarSEngine';
+import { switchCarSEngineToDriveMode } from '../API/switchCarSEngineToDriveMode';
+import { updateCar } from '../API/updateCar';
+import { STORE } from '../data/store';
+import { THTMLParam } from '../types/types';
 
-const renderLogo = (elem$: HTMLElement) => {
+const renderLogo = (elem$: THTMLParam) => {
   createHTMLElem(elem$, 'h1', { class: 'h1 logo col-6 col-sm-12' }, 'ASYNC RACE');
 };
 
-const renderThemeButton = (elem$: HTMLElement) => {
+const renderThemeButton = (elem$: THTMLParam) => {
   const button$ = createHTMLElem(elem$, 'button', { class: 'button button-icon theme-button' });
   createHTMLElem(button$, 'span', { class: 'theme-button__icon-from-dark' }, '☼');
   createHTMLElem(button$, 'span', { class: 'theme-button__icon-from-light' }, '☽');
@@ -13,7 +22,7 @@ const renderThemeButton = (elem$: HTMLElement) => {
   addChangeThemeListener(button$);
 };
 
-const renderMunuButtons = (elem$: HTMLElement) => {
+const renderMunu = (elem$: THTMLParam) => {
   const menu$ = createHTMLElem(elem$, 'div', { class: 'menu col-6 col-sm-12' });
   const buttonGarage$ = createHTMLElem(menu$, 'button', { class: 'button menu__garage' }, 'garage');
   const buttonWinners$ = createHTMLElem(
@@ -24,21 +33,22 @@ const renderMunuButtons = (elem$: HTMLElement) => {
   );
   renderThemeButton(menu$);
 
-  buttonGarage$.addEventListener('click', () => {
-    console.log('garage');
+  buttonGarage$.addEventListener('click', async () => {
+    const cars = await getCars(7);
+    STORE.updateSettings('cars', cars);
   });
-  buttonWinners$.addEventListener('click', () => {
-    console.log('winners');
+  buttonWinners$.addEventListener('click', async () => {
+    const result = await switchCarSEngineToDriveMode(1);
+    console.log('stopped:', result);
   });
 };
 
 export const renderHeader = () => {
   const fragment$: DocumentFragment = new DocumentFragment();
-  const row$ = createHTMLElem(fragment$, 'div', { class: 'row' });
 
-  renderLogo(row$);
-  renderMunuButtons(row$);
+  renderLogo(fragment$);
+  renderMunu(fragment$);
 
-  const headerContainer$ = document.querySelector('.header .container') as HTMLElement;
+  const headerContainer$ = document.querySelector('.header .container .row') as HTMLElement;
   headerContainer$.prepend(fragment$);
 };
