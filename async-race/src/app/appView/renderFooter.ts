ab4464a45998ec368f41c2246bb7d33b85d0d404
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-03 10:18:11 +0300

    refactor: add renderFooter

--- async-race/src/app/appView/renderFooter.ts
+++ async-race/src/app/appView/renderFooter.ts
@@ -2,33 +2,33 @@ import { createHTMLElem } from '../../asm-scripts';
 
 export const renderFooter = () => {
   const fragment$: DocumentFragment = new DocumentFragment();
-  const row$ = createHTMLElem(fragment$, 'div', { class: 'row' });
-  const footerGithubLogo$ = createHTMLElem(row$, 'div', {
-    class: 'footer__github-logo col col-3 col-md-6',
+
+  const footerGithubLogo$ = createHTMLElem(fragment$, 'div', {
+    class: 'footer__github-logo col col-2 col-sm-6',
   });
   createHTMLElem(footerGithubLogo$, 'a', {
     class: 'link github-logo',
-    atribut: 'target="_blank" href="https://github.com/AmelianceSkyMusic"',
+    attributes: 'target="_blank" href="https://github.com/AmelianceSkyMusic"',
   });
-  const footerCopyrigh$ = createHTMLElem(row$, 'div', {
-    class: 'footer__copyright col col-6 col-md-12',
+  const footerCopyrigh$ = createHTMLElem(fragment$, 'div', {
+    class: 'footer__copyright col col-8 col-sm-12',
   });
   createHTMLElem(
     footerCopyrigh$,
     'a',
     {
-      class: 'p1 link h4',
-      atribut: 'target="_blank" href="https://dribbble.com/rsayuaie"',
+      class: 'link',
+      attributes: 'target="_blank" href="https://dribbble.com/rsayuaie"',
     },
     '2022 © Designed by AmelianceSkyMusic',
   );
-  const footerRsschoolLogo$ = createHTMLElem(row$, 'div', {
-    class: 'footer__rsschool-logo col col-3 col-md-6',
+  const footerRsschoolLogo$ = createHTMLElem(fragment$, 'div', {
+    class: 'footer__rsschool-logo col col-2 col-sm-6',
   });
   createHTMLElem(footerRsschoolLogo$, 'a', {
     class: 'link rsschool-logo',
-    atribut: 'target="_blank" href="https://rs.school/js/"',
+    attributes: 'target="_blank" href="https://rs.school/js/"',
   });
-  const footerContainer$ = document.querySelector('.footer .container') as HTMLElement;
+  const footerContainer$ = document.querySelector('.footer .container .row') as HTMLElement;
   footerContainer$.prepend(fragment$);
 };
