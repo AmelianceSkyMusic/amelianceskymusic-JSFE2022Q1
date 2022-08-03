fd00a0a7f8de3381fbdb4914ada1aa78e48a14ba
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-03 17:25:18 +0300

    refactor: add init garage controller

--- async-race/src/app/garage/garageController/garageController.ts
+++ async-race/src/app/garage/garageController/garageController.ts
@@ -1,13 +1,8 @@
 import { STORE } from '../../data/store';
-import { IStore } from '../../types/interfaces';
 import { garageModel } from '../garageModel';
 import { garageView } from '../garageView';
 
 export const garageController = () => {
-  const log = (store: IStore) => {
-    console.log('update--> ', store);
-  };
-  STORE.addSubscriber(log);
+  STORE.addSubscriber(garageView);
   garageModel();
-  garageView();
 };
