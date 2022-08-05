a7abe8ae3b0979abdf97e9a9e7998933afa94b72
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-05 11:34:30 +0300

    refactor: update API updateCars

--- async-race/src/app/API/cars/updateCar.ts
+++ async-race/src/app/API/cars/updateCar.ts
@@ -1,6 +1,9 @@
 import { doFetch } from '../doFetch';
 
-export const updateCar = async (id: number, name: string, color: string) => {
+export const updateCar = async (
+  { id, name, color }:
+  { id: number, name: string, color: string },
+) => {
   const url = 'garage';
 
   const paramsString = id > 0 ? `/${id}` : '';
