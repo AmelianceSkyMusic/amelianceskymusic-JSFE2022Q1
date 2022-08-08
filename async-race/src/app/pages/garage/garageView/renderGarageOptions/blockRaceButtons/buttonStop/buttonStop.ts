0b2b986e152a84440aa68110be11676f1edc22d7
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-08 11:28:38 +0300

    refactor: add loader

--- async-race/src/app/pages/garage/garageView/renderGarageOptions/blockRaceButtons/buttonStop/buttonStop.ts
+++ async-race/src/app/pages/garage/garageView/renderGarageOptions/blockRaceButtons/buttonStop/buttonStop.ts
@@ -1,4 +1,5 @@
 import { createHTMLElem } from '../../../../../../../asm-scripts';
+import ASMUI from '../../../../../../../asm-ui/scripts/ASMUI';
 import API from '../../../../../../API';
 import { ICar, IStore } from '../../../../../../types/interfaces';
 import { THTMLParam } from '../../../../../../types/types';
@@ -7,24 +8,28 @@ import { carStop } from '../../../action/carStop';
 import { disableStopAllButton } from '../../../disableBoxTrackControls/disableStopAllButton';
 import { removeWinnerAlert } from '../../../winnerAlert/removeWinnerAlert';
 
-export const carStopEngine = async (id: number) => {
-  await API.stopCarSEngine(id);
-};
+// export const carStopEngine = async (id: number) => {
+//   await API.stopCarSEngine(id);
+// };
 
 export const buttonStop = async (elem$: THTMLParam, store: IStore) => {
   createHTMLElem(elem$, 'button', {
     class: 'button button-sm race-buttons__reset',
   }, 'RESET')
-    .addEventListener('click', () => {
+    .addEventListener('click', async () => {
+      const blackout$ = ASMUI.renderBlackout();
+      ASMUI.renderLoader(blackout$);
       disableStopAllButton();
+
       const cars = store.cars as ICar[];
       cars.forEach(async (car) => {
-        await carStopEngine(car.id);
+        await API.stopCarSEngine(car.id);
       });
       cars.forEach((car) => {
         carStop(car.id);
       });
       removeWinnerAlert();
-      updateModel();
+      await updateModel();
+      ASMUI.removeBlackout(blackout$);
     });
 };
