8947a3ab6e11a09031c1ae1ef3951dd6e33bed32
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-08 09:19:57 +0300

    refactor: update doFetch

--- async-race/src/app/API/doFetch.ts
+++ async-race/src/app/API/doFetch.ts
@@ -1,24 +1,24 @@
-import { TCars } from '../types/types';
-
 export const doFetch = async (request: string, method = 'GET', headers = {}, body = {}) => {
   const serverUrl = 'http://localhost:3000/';
-  let data: TCars = [];
-  let count;
 
   const fetchParams: { [key: string]: string | number | { [key: string]: string | number } } = {};
   if (method) fetchParams.method = method;
   if (Object.keys(headers).length > 0) fetchParams.headers = headers;
   if (Object.keys(body).length > 0) fetchParams.body = JSON.stringify(body);
 
-  try {
-    const response = await fetch(serverUrl + request, fetchParams);
-    if (response.status === 404) console.log('get 404');
-    data = await response.json();
+  const response = await fetch(serverUrl + request, fetchParams);
 
-    count = response.headers.get('X-Total-Count');
-  } catch (error) {
-    throw new Error();
-  }
+  // let status: number;
+  // switch (response.status) {
+  //   case 200: status = 200; break;
+  //   case 404: status = 404; break;
+  //   case 500: status = 500; break;
+  //   default: status = -1; break;
+  // }
+
+  const data = await response.json();
+
+  const count = response.headers.get('X-Total-Count');
 
   return { data, count };
 };
