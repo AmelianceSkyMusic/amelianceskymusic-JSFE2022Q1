ba1a0eceb52848f0daf4760e5e9701a63ea6a640
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-06-01 16:30:03 +0300

    fix: update server api link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
+        super('https://newsapi.org/v2/', {
             apiKey: '2e5debdad61a4b9cb982ed04656c99f7', // получите свой ключ https://newsapi.org/
         });
     }
